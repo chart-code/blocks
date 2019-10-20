180ebff82919a867fe9b68bef764dfd46adcc3dc
Adam Pearce <adampearce@google.com>
2019-10-19 20:40:16 -0400

    deploy cmd

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-cd .. && rsync -a --omit-dir-times --no-perms --exclude node_modules --exclude .git blocks/ demo@roadtolarissa.com:blocks/
+cd .. && rsync -a --omit-dir-times --no-perms --exclude node_modules  --exclude usercache --exclude .git blocks/ demo@roadtolarissa.com:blocks/
 
 ssh demo@roadtolarissa.com <<'ENDSSH'
   #commands to run on remote host
