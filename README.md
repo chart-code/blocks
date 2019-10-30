e5e8c1e9adbef4f8913c070e663796bfda882cba
Adam Pearce <adampearce@google.com>
2019-10-29 22:52:40 -0400

    adds token query, closes #3

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Open source clone of [bl.ocks.org](https://bl.ocks.org/), hosted at [blocks.road
 `yarn && yarn dev`
 
 If you've installed [gistup](https://github.com/mbostock/gistup), http://localhost:3002/$yourUsername will include your private gists. 
+
+To view your private gists without running locally, run `cat ~/.gistup.json` to get your token, and go to `https://blocks.roadtolarissa.com/$yourUsername?token=$token`
--- routes/user.js
+++ routes/user.js
@@ -5,13 +5,13 @@ var d3 = require('d3')
 
 var e = _.escape
 
-async function dlGists(user, maxPages=11){
+async function dlGists(user, token='', maxPages=11){
   var responces = []
   var responce = null
   var page = 1
   do {
     var url = `https://api.github.com/users/${user}/gists?page=${page}&per_page=100`
-    var responce = await fetchCache(url, 'json')
+    var responce = await fetchCache(url, 'json', token)
     responces.push(responce)
     page++
   } while (responce.length == 100 && page < maxPages)
@@ -21,8 +21,8 @@ async function dlGists(user, maxPages=11){
     .filter(d => d.id)
 }
 
-async function getGists(user){
-  var path = __dirname + '/../usercache/' + user + '.csv'
+async function getGists(user, token){
+  var path = __dirname + '/../usercache/' + user + token + '.csv'
 
   var cachedGists = []
   var maxPages = 0
@@ -33,7 +33,7 @@ async function getGists(user){
   }
 
   // misses gists if someone makes a 100+ gists between caches
-  var gists = await dlGists(user, maxPages)
+  var gists = await dlGists(user, token, maxPages)
 
   var isId = {}
   gists.forEach(d => isId[d.id] = true)
@@ -42,7 +42,7 @@ async function getGists(user){
 
   // download & save full list of gists after making request 
   !(async function(){
-    var currentGists = await dlGists(user)
+    var currentGists = await dlGists(user, token)
     if (currentGists.length > 0) io.writeData(path, currentGists, d => d)
   })()
 
@@ -75,7 +75,8 @@ function generateHTML(user, gists){
 
 module.exports = async function get(req, res, next) {
   var user = req.params.user
-  var gists = await getGists(req.params.user)
+  var token = req.query.token || ''
+  var gists = await getGists(req.params.user, token)
 
   // redirect if user doesn't exist and there's a gist id with their user name
   // /397f1b0905400b83fcea4008fb4ccdb1 -> /1wheel/397f1b0905400b83fcea4008fb4ccdb1
