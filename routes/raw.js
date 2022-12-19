2edbfd011d6cf4583be81f40c220509a9810879a
Philippe Rivière <fil@rezo.net>
2022-12-19 11:41:41 +0100

    fix images
    closes https://github.com/chart-code/blocks/issues/4

--- routes/raw.js
+++ routes/raw.js
@@ -1,24 +1,31 @@
-var fetchCache = require('./../lib/fetch-cache')
-var mime = require('mime-types')
-
+const fetch = require("node-fetch");
+const fetchCache = require("./../lib/fetch-cache");
+const mime = require("mime-types");
 
 module.exports = async function get(req, res, next) {
-  var {user, id, file} = req.params
+  const { user, id, file } = req.params;
 
-  var url =`https://gist.githubusercontent.com/${user}/${id}/raw/${file}`
+  const url = `https://gist.githubusercontent.com/${user}/${id}/raw/${file}`;
+  try {
+    const type = mime.lookup(file);
+    if (type.match(/^image/)) {
+      fetch(url).then(async (resp) => {
+        resp.headers.forEach((v, n) => res.setHeader(n, v));
+        resp.body.pipe(res);
+      });
+    } else {
+      let text = await fetchCache(url, "text");
+      if (file.includes(".html")) text = text.replace(/http:\/\//g, "//");
 
-  try{
-    var text = await fetchCache(url, 'text')
-    if (file.includes('.html')) text = text.replace(/http:\/\//g, '//')
-
-    res.writeHead('200', {
-      'Content-Type': mime.lookup(file),
-      'Cache-Control': 'public, max-age=' + 1000*60,
-    })
-    res.end(text)
-  } catch(e){
-    console.log('missing', e)
-    res.writeHead('404')
-    res.end('')
+      res.writeHead("200", {
+        "Content-Type": mime.lookup(file),
+        "Cache-Control": "public, max-age=" + 1000 * 60,
+      });
+      res.end(text);
+    }
+  } catch (e) {
+    console.log("missing", e);
+    res.writeHead("404");
+    res.end("");
   }
-}
+};
